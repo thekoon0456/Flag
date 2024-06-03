a95fa7154c45ccccf792a752310e326d7c10f0d8
deokhunkim <thekoon0456@gmail.com>
2024-06-03 19:56:30 +0900

    [Feat] 프로젝트 구성

---
+++ Flag/Flag/FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagApp.swift
+//  Flag
+//
+//  Created by Deokhun KIM on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
