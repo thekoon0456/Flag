5d4c2885b76796fbb7b580a7695f22642a20e2f3
deokhunkim <thekoon0456@gmail.com>
2024-06-30 02:06:36 +0900

    [Feat] Test코드 추가

--- Flag/Flag/Presenter/TestTCA/CounterFeature.swift
+++ Flag/Flag/Presenter/TestTCA/CounterFeature.swift
@@ -29,6 +29,9 @@ struct CounterFeature {
     
     enum CancelID { case timer }
     
+    @Dependency(\.continuousClock) var clock
+    @Dependency(\.numberFact) var numberFact
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
@@ -54,8 +57,9 @@ struct CounterFeature {
                 //                state.isLoading = false
                 //                return .none
                 return .run { [count = state.count] send in
-                    let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
-                    let fact = String(decoding: data, as: UTF8.self)
+//                    let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+//                    let fact = String(decoding: data, as: UTF8.self)
+                    let fact = try await numberFact.fetch(count)
                     await send(.factResponse(fact: fact)) //send: mainActor
                 }
                 
